3a696aabfa345f680edcd4b20aac751fab549742
nadinnah <nadinahmedmohamed@gmail.com>
2026-01-12 00:24:01 +0300

    need to fix order redirection

--- taco-cloud-application/taco-cloud/src/main/java/com/example/tacocloud/repositories/TacoRepository.java
+++ taco-cloud-application/taco-cloud/src/main/java/com/example/tacocloud/repositories/TacoRepository.java
@@ -3,7 +3,6 @@ package com.example.tacocloud.repositories;
 import com.example.tacocloud.models.Taco;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TacoRepository {
     Taco save(Taco design);
 }

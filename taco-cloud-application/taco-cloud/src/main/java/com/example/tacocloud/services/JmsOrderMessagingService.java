dfeabec9d1cc793c227e3e1cd6a117b781f0565f
nadinnah <nadinahmedmohamed@gmail.com>
2026-02-03 23:34:25 +0300

    add JmsOrderMessagingService

---
+++ taco-cloud-application/taco-cloud/src/main/java/com/example/tacocloud/services/JmsOrderMessagingService.java
@@ -0,0 +1,11 @@
+package com.example.tacocloud.services;
+
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JmsOrderMessagingService {
+    private JmsTemplate jms;
+
+
+}

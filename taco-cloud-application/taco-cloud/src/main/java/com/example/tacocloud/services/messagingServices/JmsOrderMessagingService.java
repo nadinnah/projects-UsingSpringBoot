9d56aa1b09053caf064f2a19e1c2e584166fc9f0
nadinnah <nadinahmedmohamed@gmail.com>
2026-02-06 13:37:35 +0300

    adding pull and push models for jms

--- taco-cloud-application/taco-cloud/src/main/java/com/example/tacocloud/services/messagingServices/JmsOrderMessagingService.java
+++ taco-cloud-application/taco-cloud/src/main/java/com/example/tacocloud/services/messagingServices/JmsOrderMessagingService.java
@@ -1,36 +1,38 @@
-package com.example.tacocloud.services;
+package com.example.tacocloud.services.messagingServices;
 
 import com.example.tacocloud.models.Order;
 import jakarta.jms.Destination;
 import jakarta.jms.JMSException;
 import jakarta.jms.Message;
-import jakarta.jms.Session;
-import org.apache.activemq.artemis.jms.client.ActiveMQQueue;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
-import org.springframework.jms.core.MessagePostProcessor;
+import org.springframework.jms.support.converter.JacksonJsonMessageConverter;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@Service
+//JMS has a few shortcomings, not the least of which is that as a Java specification its use
+//is limited to Java applications
+
+//RabbitMQ and Kafka address these shortcomings and are available for other languages and platforms beyond the JVM
+
+@Component
 public class JmsOrderMessagingService implements OrderMessagingService {
     private JmsTemplate jms;
-
     private Destination orderQueue;
+    private JacksonJsonMessageConverter messageConverter;
 
     @Autowired
-    public JmsOrderMessagingService(JmsTemplate jms, Destination orderQueue){
+    public JmsOrderMessagingService(JmsTemplate jms, Destination orderQueue, JacksonJsonMessageConverter messageConverter){
+        this.messageConverter=messageConverter;
         this.jms=jms;
         this.orderQueue= orderQueue;
     }
 
 
-    private Message addOrderSource(Message message) throws JMSException{
-        message.setStringProperty("X_ORDER_SOURCE", "WEB");
-        return message;
-    }
+//    private Message addOrderSource(Message message) throws JMSException{
+//        message.setStringProperty("X_ORDER_SOURCE", "WEB");
+//        return message;
+//    }
 
 
 

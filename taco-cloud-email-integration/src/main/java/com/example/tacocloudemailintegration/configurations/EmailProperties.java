374a5e13965f133f04a5634fa4b39aa448022ac9
nadinnah <nadinahmedmohamed@gmail.com>
2026-02-17 17:38:20 +0300

    add emailIntegration application for taco cloud

--- taco-cloud-email-integration/src/main/java/com/example/tacocloudemailintegration/configurations/EmailProperties.java
+++ taco-cloud-email-integration/src/main/java/com/example/tacocloudemailintegration/configurations/EmailProperties.java
@@ -1,4 +1,5 @@
-package com.example.tacocloud.configurationPropertyHolders;
+package com.example.tacocloudemailintegration.configurations;
+
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -13,9 +14,10 @@ public class EmailProperties {
     private String host;
     private String mailbox;
     private long pollRate = 30000;
-    
+
     public String getImapUrl() {
         return String.format("imaps://%s:%s@%s/%s",
                 this.username, this.password, this.host, this.mailbox);
     }
 }
+
---
+++ taco-cloud-email-integration/src/test/java/com/example/tacocloudemailintegration/TacoCloudEmailIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudemailintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudEmailIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

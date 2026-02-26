ffb25959224ee4c90552ed91f27a1d8ba3057007
nadinnah <nadinahmedmohamed@gmail.com>
2026-02-26 21:46:02 +0300

    add react and spring project

---
+++ reactive-coding-interview-platform/src/main/java/com/example/reactivecodinginterviewplatform/ReactiveCodingInterviewPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactivecodinginterviewplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveCodingInterviewPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveCodingInterviewPlatformApplication.class, args);
+    }
+
+}

8b9a26b592fbf6bf0e82117040bca1b6f5f0451a
nadinnah <nadinahmedmohamed@gmail.com>
2026-03-04 23:53:23 +0300

    add flashcard project

---
+++ flashcard-fullstack/src/main/java/com/example/flashcardfullstack/FlashcardFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.example.flashcardfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardFullstackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashcardFullstackApplication.class, args);
+    }
+
+}

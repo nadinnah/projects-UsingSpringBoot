ec07d2bd356eb81bef2d4cccdfec2cd1a49c510b
nadinnah <nadinahmedmohamed@gmail.com>
2026-03-06 03:25:29 +0300

    add flashcard logic in react

--- React/flashcard-fullstack/src/App.js
+++ React/flashcard-fullstack/src/App.js
@@ -1,13 +1,32 @@
-import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import FlashcardList from "./component/FlashcardList";
+import "./App.css"
 import Navbar from './layout/Navbar';
-
+import React, {useState} from "react";
 function App() {
+
+  const[flashcards, setFlashcards]= useState(SAMPLE_FLASHCARDS)
+
   return (
     <div className="App">
       <Navbar/>
+      <FlashcardList flashcards={flashcards}/>
+
     </div>
   );
 }
+
+const SAMPLE_FLASHCARDS=[
+  {
+    id:1,
+    frontText: "Hallo",
+    backText:"Hello"
+  },
+  {
+    id:2,
+    frontText: "Frau",
+    backText:"Woman"
+  }
+]
 
 export default App;
---
+++ React/flashcard-fullstack/src/component/Flashcard.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+
+const Flashcard = ({flashcard}) => {
+    const [flip, setFlip]= useState(false)
+
+    return (
+        <div className={`card ${flip? 'flip' :''}`} 
+        onClick={()=>setFlip(!flip)}
+        >
+            <div className='front'>
+                {flashcard.frontText}
+            </div>
+            <div className='back'>
+                {flashcard.backText}
+            </div>
+        </div>
+    );
+}
+
+export default Flashcard;
